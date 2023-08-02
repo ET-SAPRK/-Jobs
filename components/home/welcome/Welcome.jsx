35ced3d3184e6f20ea29bbd2744679eca00c53d0
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-02 18:24:58 +0300

    edited

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
 import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 import { TextInput } from 'react-native-gesture-handler';
 
 const Welcome = () => {
+  const jobTypes = ["Full-time", "Part-time", "Contractor"];
   const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
      <View style={styles.container}>
@@ -30,6 +32,25 @@ const Welcome = () => {
           />
         </TouchableOpacity>
      </View>
+     <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
     </View>
   )
 }
